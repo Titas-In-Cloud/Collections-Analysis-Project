848cac3fa7172580634d21b3e8e05734c6367e7e
b9016816 <b9016816@newcastle.ac.uk>
2020-02-20 09:57:19 +0000

    Added new input method which asks user to which directory he wants to add entries. Fixed bad input loop when checking if extension is a five digit number.

--- Input.java
+++ Input.java
@@ -6,86 +6,109 @@ import java.util.Scanner;
 
 public class Input extends MainClass{
 
-    public void userInputCheck(Directory directory1, Directory directory2, Directory directory3){
+    Scanner userInput = new Scanner(System.in);
 
-        Scanner user_input = new Scanner(System.in);
+    public Directory whichDirectory(){
+
+        String directoryName;
+
+        System.out.print("To which directory you would like to insert your entries? ");
+        directoryName = userInput.next();
+
+        while (!directoryName.equals("Array") && !directoryName.equals("array")
+                && !directoryName.equals("ArrayList") && !directoryName.equals("arraylist")
+                && !directoryName.equals("HashMap") && !directoryName.equals("hashmap")) {
+            System.out.print("Error! Please choose 'Array', 'ArrayList' or 'HashMap'. ");
+            directoryName = userInput.next();
+        }
+        switch (directoryName) {
+            case "Array":
+                return new ArrayDirectory();
+            case "array":
+                return new ArrayDirectory();
+            case "ArrayList":
+                return new ArrayListDirectory();
+            case "arraylist":
+                return new ArrayListDirectory();
+            case "HashMap":
+                return new HashMapDirectory();
+            case "hashmap":
+                return new HashMapDirectory();
+            default:
+                return null;
+        }
+    }
+
+    public void userInputCheck(Directory directory){
 
         String inputAnswer, moreInput;
 
         System.out.print("Do you want to insert any entries to directory? ");
-        inputAnswer = user_input.next();
+        inputAnswer = userInput.next();
 
         // checks if answer was Yes or No. Returns Error if it was not.
-        while(true){
-            if(inputAnswer.equals("Yes") || inputAnswer.equals("yes") ||
-                    inputAnswer.equals("No") || inputAnswer.equals("no")) {
-                break;
-            }
+        while (!inputAnswer.equals("Yes") && !inputAnswer.equals("yes") &&
+                !inputAnswer.equals("No") && !inputAnswer.equals("no")) {
             System.out.print("Error! Please choose 'Yes' or 'No'. ");
-            inputAnswer = user_input.next();
+            inputAnswer = userInput.next();
         }
 
         if(inputAnswer.equals("Yes") || inputAnswer.equals("yes")){
 
             // calls entry input method
-            userInput(directory1, directory2, directory3);
+            userInput(directory);
 
             while(true){
 
-                System.out.println("Do you want to insert one more entry to directory? ");
-                moreInput = user_input.next();
+                System.out.print("Do you want to insert one more entry? ");
+                moreInput = userInput.next();
 
                 // checks if answer was Yes or No. Returns Error if it was not.
-                while(true){
-                    if(moreInput.equals("Yes") || moreInput.equals("yes") ||
-                            moreInput.equals("No") || moreInput.equals("no")) {
-                        break;
-                    }
+                while (!moreInput.equals("Yes") && !moreInput.equals("yes") &&
+                        !moreInput.equals("No") && !moreInput.equals("no")) {
                     System.out.print("Error! Please choose 'Yes' or 'No'. ");
-                    moreInput = user_input.next();
+                    moreInput = userInput.next();
                 }
 
                 // calls entry input method if user wants to add more entries to directories
                 // breaks loop if user does not want to add more entries to directories
                 if(moreInput.equals("Yes") || moreInput.equals("yes")){
-                    userInput(directory1, directory2, directory3);
+                    userInput(directory);
                 } else break;
             }
         }
     }
 
-    public void userInput(Directory directory1, Directory directory2, Directory directory3) {
+    public void userInput(Directory directory) {
 
         String surname, initials, number;
 
-        Scanner user_input = new Scanner(System.in);
-
         System.out.println("Please input the following.");
         System.out.print("Surname: ");
-        surname = user_input.next();
+        surname = userInput.next();
         System.out.print("Initials: ");
-        initials = user_input.next();
+        initials = userInput.next();
         System.out.print("Extension number: ");
-        number = user_input.next();
+        number = userInput.next();
 
-        while(number.length() != 5){
+        while(!number.matches("\\d+") || number.length() != 5){
             System.out.print("Error! Please input a number with 5 digits: ");
-            number = user_input.next();
+            number = userInput.next();
         }
 
         Entry newEntry = new Entry();
         newEntry.setEntry(surname, initials, number);
 
-        directory1.insertEntry(newEntry);
-        directory2.insertEntry(newEntry);
-        directory3.insertEntry(newEntry);
+        directory.insertEntry(newEntry);
     }
 
-    public void readerCSV(Directory directory1, Directory directory2, Directory directory3) {
+    public void readerCSV(Directory directory) {
         String csvFile = "test_data.csv";
         BufferedReader br = null;
         String line = "";
         String variablesSplitBy = ",";
+
+        StopWatch stopWatch = new StopWatch();
 
         try{
             br = new BufferedReader(new FileReader(csvFile));
@@ -95,9 +118,7 @@ public class Input extends MainClass{
                 Entry newEntry = new Entry();
                 newEntry.setEntry(entry[0], entry[1], entry[2]);
 
-                directory1.insertEntry(newEntry);
-                directory2.insertEntry(newEntry);
-                directory3.insertEntry(newEntry);
+                directory.insertEntry(newEntry);
             }
 
         } catch (IOException e){
