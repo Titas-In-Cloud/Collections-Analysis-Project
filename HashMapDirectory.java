0d07fa24472fef197da9139a0f7d45870f16fa1d
b9016816 <b9016816@newcastle.ac.uk>
2020-02-20 14:41:05 +0000

    Modified HashMapDirectory.java file to be faster. Added 2 HashMap rather than 1 - one for surname and another for number.

--- HashMapDirectory.java
+++ HashMapDirectory.java
@@ -4,66 +4,46 @@ import java.util.List;
 
 public class HashMapDirectory implements Directory {
 
-    int index = 0;
-
-    HashMap<Integer,Entry> hashMapDirectory = new HashMap<>();
+    HashMap<String,Entry> hashMapDirectorySurname = new HashMap<>();
+    HashMap<String,Entry> hashMapDirectoryNumber = new HashMap<>();
 
     public void insertEntry(Entry entry) {
 
-        // puts entry to a hash map in an index of index
-        hashMapDirectory.put(index, entry);
-        index++;
+        // puts new entry to 2 hash maps - one with an index of surname and another of number
+        hashMapDirectorySurname.put(entry.getSurname(), entry);
+        hashMapDirectoryNumber.put(entry.getNumber(), entry);
 
     }
 
     public void deleteEntryUsingName(String surname) {
 
-        // removes entry from the hash map by required surname
-        for(int i = 0; i < hashMapDirectory.size(); i++){
-            if(hashMapDirectory.get(i).getSurname().equals(surname)){
-                hashMapDirectory.remove(i);
-            }
-        }
+        hashMapDirectoryNumber.remove(hashMapDirectorySurname.get(surname).getNumber());
+        hashMapDirectorySurname.remove(surname);
+
     }
 
     public void deleteEntryUsingExtension(String number) {
 
-        // removes entry from the array list by required extension number
-        for(int i = 0; i < hashMapDirectory.size(); i++){
-            if(hashMapDirectory.get(i).getNumber().equals(number)){
-                hashMapDirectory.remove(i);
-            }
-        }
+        hashMapDirectorySurname.remove(hashMapDirectoryNumber.get(number).getSurname());
+        hashMapDirectoryNumber.remove(number);
+
     }
 
     public void updateExtensionUsingName(String surname, String newNumber) {
 
-        // searches for the entry with required surname and updates it's extension
-        for(int i = 0; i < hashMapDirectory.size(); i++){
-            if(hashMapDirectory.get(i).getSurname().equals(surname)){
-                // changes extension to a new extension which was converted to integer
-                hashMapDirectory.get(i).setNumber(newNumber);
-            }
-        }
+        hashMapDirectoryNumber.get(hashMapDirectorySurname.get(surname).getNumber()).setNumber(newNumber);
+        hashMapDirectorySurname.get(surname).setNumber(newNumber);
+
     }
 
     public String lookupExtension(String surname) {
 
-        String numberExtension = null;
-
-        // searches for the required surname and saves it's extension
-        for(int i = 0; i < hashMapDirectory.size(); i++){
-            if(hashMapDirectory.get(i).getSurname().equals(surname)){
-                numberExtension = hashMapDirectory.get(i).getNumber();
-            }
-        }
-
-        return numberExtension;
+        return hashMapDirectorySurname.get(surname).getNumber();
     }
 
     public List<Entry> toArrayList() {
 
         // creates and returns a list with all the objects of the array list;
-        return new ArrayList<>(hashMapDirectory.values());
+        return new ArrayList<>(hashMapDirectorySurname.values());
     }
 }
