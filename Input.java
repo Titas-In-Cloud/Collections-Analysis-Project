6385fa3b2f573d91dda6de7945666bf234001973
b9016816 <b9016816@newcastle.ac.uk>
2020-02-21 03:07:48 +0000

    Made user menu which can be controlled via terminal.

--- Input.java
+++ Input.java
@@ -9,33 +9,77 @@ public class Input extends MainClass{
 
     Scanner userInput = new Scanner(System.in);
 
-    /*public void userMenu(){
+    Output output = new Output();
 
-        String Answer;
+    PerformanceAnalysis performanceAnalysis = new PerformanceAnalysis();
 
-        System.out.println("What would you like to do? " +
-                "Input 'menu' to get a list of available actions.");
-        Answer = userInput.next();
+    public void userMenu() throws IOException {
 
-        while(Answer.equals("End") || Answer.equals("end")){
+        String answer;
+        String directoryName = null;
+        Directory directory = null;
+        boolean firstCommand = true;
 
-            if(Answer == "Menu" || Answer == "menu"){
-                System.out.println("Action menu:");
-                System.out.println(" - menu - returns a menu of available actions.");
-                System.out.println(" - input - ");
-                System.out.println(" - delete by name - ");
-                System.out.println(" - delete by extension - ");
-                System.out.println(" - update extension - ");
-                System.out.println(" - lookup extension - ");
-                System.out.println(" - - ");
-                System.out.println(" - csv - ");
-                System.out.println(" - table - ");
-                System.out.println(" - analysis - ");
+        System.out.print("What would you like to do? " +
+                "Input 'menu' to get a list of available actions. ");
+        answer = userInput.next();
+
+        while(true) {
+            if(answer.equals("directory") || answer.equals("Directory")){
+                directory = whichDirectory();
+                if(directory.toString().contains("ArrayDirectory")) { directoryName = "Array"; }
+                if(directory.toString().contains("ArrayListDirectory")) { directoryName = "Array List"; }
+                if(directory.toString().contains("HashMapDirectory")) { directoryName = "HashMap"; }
+                System.out.println("You chosen " + directoryName + " directory.");
             }
-            Answer = userInput.next();
+            else if(answer.equals("menu") || answer.equals("Menu")) {
+                System.out.println("Action menu:");
+                System.out.println(" - directory - sets which directory to initialize for usage.");
+                System.out.println(" - input - starts manual input of entries to directory.");
+                System.out.println(" - csvIn - reads CSV file and puts all the entries to the directory.");
+                System.out.println(" - csvOut - puts all the directory entries to CSV file.");
+                System.out.println(" - table - returns a table with entries in ASCII format.");
+                System.out.println(" - analysis - runs method analysis of Array, Array List and HashMap directories.");
+                System.out.println(" - exit - ends the program.");
+            }
+            else if(answer.equals("exit") || answer.equals("Exit")){
+                System.out.println("Program was closed.");
+                break;
+            }
+            else if(answer.equals("analysis") || answer.equals("Analysis")){
+                performanceAnalysis.directoryPerformance("Array");
+                performanceAnalysis.directoryPerformance("ArrayList");
+                performanceAnalysis.directoryPerformance("HashMap");
+                System.out.println("Analysis was done successfully and " +
+                        "txt files were created with method running times.");
+        }
+            else if((answer.equals("input") || answer.equals("Input")) && directory != null) {
+                userInputCheck(directory);
+            }
+            else if((answer.equals("csvin") || answer.equals("csvIn")) && directory != null){
+                readerCSV(directory, false);
+                System.out.println("Entries from CSV file were successfully put to the " + directoryName + " directory.");
+            }
+            else if((answer.equals("csvout") || answer.equals("csvOut")) && directory != null){
+                output.writerCSV(directory);
+                System.out.println("CSV file was successfully created with entries from " + directoryName + " directory.");
+            }
+            else if((answer.equals("table") || answer.equals("Table")) && directory != null){
+                output.outputInTable(directory);
+            }
+            else if(firstCommand) {
+                System.out.println("Error! Please input 'menu' to get menu of available actions. ");
+                firstCommand = false;
+            }
+            else if(directory == null) {
+                System.out.println("Please input 'directory' to initialize directory first.");
+            }
+            else System.out.println("Error! Please input 'menu' to get menu of available actions. ");
+
+            System.out.print("Action: ");
+            answer = userInput.next();
         }
     }
-     */
 
     public Directory whichDirectory(){
 
@@ -139,19 +183,12 @@ public class Input extends MainClass{
 
         double executionTime;
 
-        JFileChooser fileChooser = new JFileChooser();
-
-        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-        fileChooser.setDialogTitle("Select CSV File To Read");
-        fileChooser.showSaveDialog(null);
-
-        String csvFile = fileChooser.getSelectedFile().getName();
         BufferedReader br = null;
-        String line = "";
         String variablesSplitBy = ",";
+        String line;
 
         try{
-            br = new BufferedReader(new FileReader(csvFile));
+            br = new BufferedReader(new FileReader("test_data.csv"));
             while ((line = br.readLine()) != null){
                 String[] entry = line.split(variablesSplitBy);
 
