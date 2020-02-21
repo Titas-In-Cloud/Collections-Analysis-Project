bf379b85cea702b3d8c3f87b7bb19eed45cac164
b9016816 <b9016816@newcastle.ac.uk>
2020-02-21 03:13:35 +0000

    Updated MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -3,21 +3,9 @@ import java.io.IOException;
 public class MainClass {
     public static void main(String[] args) throws IOException {
 
-        Directory arrayDirectory = new ArrayDirectory();
-        Directory arrayListDirectory = new ArrayListDirectory();
-        Directory hashMapDirectory = new HashMapDirectory();
-
         Input input = new Input();
-        Output output = new Output();
 
-        // calls method which checks if user wants to add any entries to directory manually
-        input.userInputCheck(arrayDirectory, arrayListDirectory, hashMapDirectory);
-
-        // calls a method which adds all entries from csv file to directories
-        input.readerCSV(arrayDirectory, arrayListDirectory, hashMapDirectory);
-
-        output.outputInTable(hashMapDirectory);
-        output.writerCSV(arrayListDirectory);
-
+        // calls user menu method from where the methods can be called
+        input.userMenu();
     }
 }
